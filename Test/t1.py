d31b26eaf60a3a05a264da395d0c3e06aa32f633
Tanaboon Jewriyavetch <6410615055@cn332>
2023-05-29 14:00:55 +0700

    temp commit

--- Test/t1.py
+++ Test/t1.py
@@ -1,35 +1,45 @@
-#t1.py
-import pathlib
+#!/usr/bin/python3
 import tkinter as tk
 import tkinter.ttk as ttk
-import pygubu
 import main_Temp as mt
-
-PROJECT_PATH = pathlib.Path(__file__).parent
-PROJECT_UI = PROJECT_PATH / "t1.ui"
 
 
 class t1:
     def __init__(self, master=None):
-        # 1: Create a builder and setup resources path (if you have images)
-        self.builder = builder = pygubu.Builder()
-        builder.add_resource_path(PROJECT_PATH)
+        # build ui
+        frame1 = tk.Frame(master)
+        frame1.configure(height=720, width=1280)
+        labelframe1 = ttk.Labelframe(frame1)
+        labelframe1.configure(height=200, text='labelframe1', width=200)
+        self.entry_directory = ttk.Entry(labelframe1)
+        self.entry_directory.configure(justify="center")
+        _text_ = 'Generated_Problems'
+        self.entry_directory.delete("0", "end")
+        self.entry_directory.insert("0", _text_)
+        self.entry_directory.pack(side="top")
+        self.entry_pages = ttk.Entry(labelframe1)
+        self.entry_pages.configure(justify="center")
+        _text_ = 'Pages'
+        self.entry_pages.delete("0", "end")
+        self.entry_pages.insert("0", _text_)
+        self.entry_pages.pack(side="top")
+        self.checkbox_pdf = ttk.Checkbutton(labelframe1)
+        global pdf
+        pdf  = tk.StringVar()
+        self.checkbox_pdf.configure(variable=pdf,offvalue="n", onvalue="y", text='PDF?')
+        self.checkbox_pdf.pack(side="top")
+        self.Generate = ttk.Button(labelframe1)
+        self.Generate.configure(text='Generate', command=self.run_main())
+        self.Generate.pack(side="top")
+        labelframe1.pack(side="top")
+        frame1.pack(side="top")
 
-        # 2: Load an ui file
-        builder.add_from_file(PROJECT_UI)
-
-        # 3: Create the mainwindow
-        self.mainwindow = builder.get_object('frame1', master)
-
-        # 4: Connect callbacks
-        builder.connect_callbacks(self)
-        self.builder.get_object("Generate").configure(command=self.run_main)
-    
+        # Main widget
+        self.mainwindow = frame1
+        
     def run_main(self):
-        directory = self.builder.get_object("entry_directory").get()
-        times = int(self.builder.get_object("entry_pages").get())
-        pdf = self.builder.get_object("checkbox_pdf").get()
-        print(pdf)
+        directory = self.entry_directory.get()
+        times = int(self.entry_pages.get())
         
         mt.main(directory, times, pdf)
 
@@ -37,7 +47,7 @@ class t1:
         self.mainwindow.mainloop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = tk.Tk()
-    app = t1()
+    app = t1
     app.run()
